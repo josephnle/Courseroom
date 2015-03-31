44e74b3966229b919b9a26c561c0983b3fe3c8b7
Joseph Le <josephle@me.com>
2015-03-31 00:22:02 -0700

    Updated user schema

--- models/User.js
+++ models/User.js
@@ -1,39 +1,66 @@
 // { "User" : "models/user.js" }
-User = new Mongo.Collection('user.js');
+userSchema = {};
 
-User.attachSchema(
-    new SimpleSchema({
-    first_name: {
-      type: String,
-      label: "First Name"
-    },
-    last_name: {
-      type: String,
-      label: "Last Name"
-    },
-    email:{
-      type: String,
-      label: "Email Address"
-    },
-    loginAt: {
-      type: Date,
-      denyUpdate: true
-    }
-  })
-);
+userSchema.UserProfile = new SimpleSchema({
+  firstName: {
+    type: String,
+    regEx: /^[a-zA-Z-]{2,25}$/,
+    optional: true
+  },
+  lastName: {
+    type: String,
+    regEx: /^[a-zA-Z]{2,25}$/,
+    optional: true
+  },
+  phone: {
+    type: String,
+    optional: true
+  }
+});
 
-// Collection2 already does schema checking
-// Add custom permission rules if needed
-if (Meteor.isServer) {
-  User.allow({
-    insert : function () {
-      return true;
-    },
-    update : function () {
-      return true;
-    },
-    remove : function () {
-      return true;
-    }
-  });
-}
+userSchema.User = new SimpleSchema({
+  username: {
+    type: String,
+    regEx: /^[a-z0-9A-Z_]{3,15}$/
+  },
+  emails: {
+    type: [Object],
+    // this must be optional if you also use other login services like facebook,
+    // but if you use only accounts-password, then it can be required
+    optional: true
+  },
+  "emails.$.address": {
+    type: String,
+    regEx: SimpleSchema.RegEx.Email
+  },
+  "emails.$.verified": {
+    type: Boolean
+  },
+  createdAt: {
+    type: Date
+  },
+  profile: {
+    type: userSchema.UserProfile,
+    optional: true
+  },
+  services: {
+    type: Object,
+    optional: true,
+    blackbox: true
+  },
+  // Add `roles` to your schema if you use the meteor-roles package.
+  // Option 1: Object type
+  // If you specify that type as Object, you must also specify the
+  // `Roles.GLOBAL_GROUP` group whenever you add a user to a role.
+  // Example:
+  // Roles.addUsersToRoles(userId, ["admin"], Roles.GLOBAL_GROUP);
+  // You can't mix and match adding with and without a group since
+  // you will fail validation in some cases.
+  roles: {
+    type: Object,
+    optional: true,
+    blackbox: true
+  }
+});
+
+Meteor.users.attachSchema(userSchema.User);
