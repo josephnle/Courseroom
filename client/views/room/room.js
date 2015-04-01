c0a03f190bb816d191a3193b73c4a72e9b6b38e8
Joseph Le <josephle@me.com>
2015-03-31 17:20:33 -0700

    Added message sending

--- client/views/room/room.js
+++ client/views/room/room.js
@@ -2,4 +2,21 @@ Template['room'].helpers({
 });
 
 Template['room'].events({
+  'keypress #sendMessageForm #messageText': function (event) {
+    if (event.which !== 13)
+      return true;
+
+    var text = event.target.value;
+
+    Messages.insert({
+      message: text,
+      room: Router.current().params._id
+    });
+
+    // Clear form
+    event.target.value = '';
+
+    // Prevent default form submit
+    return false;
+  }
 });
