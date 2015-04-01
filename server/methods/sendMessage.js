abf899bcbc09840a0b86f9fb155aff1991614d29
Joseph Le <josephle@me.com>
2015-03-31 21:38:51 -0700

    Fixed sendMessage method

--- server/methods/sendMessage.js
+++ server/methods/sendMessage.js
@@ -1,25 +1,20 @@
-function parse(thismessage)
-{
-	var res = thismessage.split(":");
-	if("Q"==res[0])
-	{
-		//trims off the Q:
-		var quest = thismessage.substr(2);
-		Questions.insert({content:quest});
-	}
-	else if("A"==res[0])
-	{
-		//trims off the A:
-		var ans = thismessage.substr(2);
-		Answers.insert({content:ans});
-	}
-	else
-	{
-		//assign original to message
-		Messages.insert({message:thismessage});
-	}
-}
-Meteor.sendMessage(function(mess_age))
-{
-	parse(mess_age);
-}
+Meteor.methods({
+  sendMessage: function(msg) {
+    check(msg, String);
+
+    var res = msg.split(":");
+    if ("Q" == res[0]) {
+      //trims off the Q:
+      var quest = msg.substr(2);
+      Questions.insert({content: quest});
+    }
+    else if ("A" == res[0]) {
+      //trims off the A:
+      var ans = msg.substr(2);
+      Answers.insert({content: ans});
+    }
+
+    // store as messge as well
+    Messages.insert({message: msg});
+  }
+});
