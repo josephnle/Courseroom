7e1f9f2daac342b1c8f003c392150a17faeb732d
Michael Mullen <mullen326@gmail.com>
2015-03-31 21:24:50 -0700

    Parser

--- server/methods/sendMessage.js
+++ server/methods/sendMessage.js
@@ -1,1 +1,25 @@
-// Write method here
+function parse(thismessage)
+{
+	var res = thismessage.split(":");
+	if("Q"==res[0])
+	{
+		//trims off the Q:
+		var quest = thismessage.substr(2);
+		Questions.insert({content:quest});
+	}
+	else if("A"==res[0])
+	{
+		//trims off the A:
+		var ans = thismessage.substr(2);
+		Answers.insert({content:ans});
+	}
+	else
+	{
+		//assign original to message
+		Messages.insert({message:thismessage});
+	}
+}
+Meteor.sendMessage(function(mess_age))
+{
+	parse(mess_age);
+}
